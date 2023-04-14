a3b0ba701085da9fb4a4293b13b6eb9e403cc95e
hrfahimm <fahimtaj10@gmail.com>
2023-04-14 10:54:17 +0600

    'dd'

---
+++ src/components/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDKnmHuKlBG76TjYLkNmVm-c0AUUQ7EaBE",
+  authDomain: "burj-al-arab-aa377.firebaseapp.com",
+  projectId: "burj-al-arab-aa377",
+  storageBucket: "burj-al-arab-aa377.appspot.com",
+  messagingSenderId: "795375984777",
+  appId: "1:795375984777:web:ed2f2b1455157f51a0e658",
+};
+export default firebaseConfig;
